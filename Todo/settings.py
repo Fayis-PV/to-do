3a4b05a9c315d2b72b0a1b8de3a5bd57e40bb562
Muhammed Fayis PV <89181651+Fayis-PV@users.noreply.github.com>
2022-12-28 19:28:30 +0530

    Update settings.py

--- Todo/settings.py
+++ Todo/settings.py
@@ -148,6 +148,12 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticroot')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+# # Configure Django App for Heroku.
+# import django_heroku
+# django_heroku.settings(locals())
+
+
+# Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
